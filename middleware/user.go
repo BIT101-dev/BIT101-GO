23e2fd9627850134644c4de64578e299ad685e1c
flwfdd <1525876733@qq.com>
2023-03-20 14:45:09 +0800

    🗝️完成注册登录模块

--- middleware/user.go
+++ middleware/user.go
@@ -1,22 +1,33 @@
 /*
  * @Author: flwfdd
  * @Date: 2023-03-13 11:52:43
- * @LastEditTime: 2023-03-15 18:13:59
+ * @LastEditTime: 2023-03-20 12:27:20
  * @Description: 用户模块中间件
  */
 package middleware
 
 import (
+	"BIT101-GO/util/config"
+	"BIT101-GO/util/jwt"
+
 	"github.com/gin-gonic/gin"
 )
 
 // 验证用户是否登录
-func CheckLogin() gin.HandlerFunc {
+func CheckLogin(strict bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fake_cookie := c.GetHeader("fake_cookie")
-		if fake_cookie != "2333" {
-			c.JSON(401, gin.H{"msg": "未登录"})
-			c.Abort()
+		token := c.GetHeader("fake-cookie")
+		c.Set("uid", 0)
+		uid, ok := jwt.VeirifyUserToken(token, config.Config.Key)
+		if ok {
+			c.Set("uid", uid)
+		} else {
+			if strict {
+				c.JSON(401, gin.H{"msg": "请先登录awa"})
+				c.Abort()
+			} else {
+				c.Set("uid", 0)
+			}
 		}
 	}
 }
