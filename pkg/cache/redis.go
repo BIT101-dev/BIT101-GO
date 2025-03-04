f1626cc2ba92181b071b68361b2ea11b2629d4a3
flwfdd <fanliwen@msh.team>
2025-03-05 02:46:17 +0800

    按照api+service+pkg重新组织包结构

---
+++ pkg/cache/redis.go
@@ -0,0 +1,25 @@
+/*
+ * @Author: flwfdd
+ * @Date: 2025-02-08 15:39:40
+ * @LastEditTime: 2025-02-08 16:02:35
+ * @Description: _(:з」∠)_
+ */
+package cache
+
+import (
+	"BIT101-GO/config"
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var RDB *redis.Client
+var Context = context.Background()
+
+func Init() {
+	// 初始化redis
+	rdb := redis.NewClient(&redis.Options{
+		Addr: config.GetConfig().Redis.Addr,
+	})
+	RDB = rdb
+}
